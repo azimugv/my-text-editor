dd1e1e1bcc028fa9bffa0545210696db54cd98db
Azim <azim210215@gmail.com>
2023-11-18 21:43:27 +0600

    TExt Italic Added

--- functions.js
+++ functions.js
@@ -2,3 +2,7 @@ document.getElementById("bold").addEventListener("click", function () {
   const textContent = getTextFieldById("text");
   textContent.style.fontWeight = "bold";
 });
+document.getElementById("italic").addEventListener("click", function () {
+  const textContent = getTextFieldById("text");
+  textContent.style.fontStyle = "italic";
+});
