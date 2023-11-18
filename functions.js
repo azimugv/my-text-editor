77e03f82c5bf23ecc29e2bc4944c30250398d252
Azim <azim210215@gmail.com>
2023-11-18 21:43:44 +0600

    Text Underline Added

--- functions.js
+++ functions.js
@@ -6,3 +6,7 @@ document.getElementById("italic").addEventListener("click", function () {
   const textContent = getTextFieldById("text");
   textContent.style.fontStyle = "italic";
 });
+document.getElementById("underline").addEventListener("click", function () {
+  const textContent = getTextFieldById("text");
+  textContent.style.textDecoration = "underline";
+});
